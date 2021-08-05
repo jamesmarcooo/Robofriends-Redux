fe8ab55ad811b316657f11d7014b0b83e88dd90d
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-05 15:35:11 +0800

    Added a class for states

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
-    return(
+class App extends Component{
+    constructor(){
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange(event){
+        console.log(event);
+    }
+
+    render(){
+        return(
         <div className='tc'>
             <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={robots}/>
+            <SearchBox onSearchChange = {this.onSearchChange}/>
+            <CardList robots={this.state.robots}/>
         </div>
-    )
+        )
+    }
+    
 }
 
 export default App;
